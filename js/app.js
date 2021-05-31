aafc7b2781345aa2cdfaf8eff850b39040698a9d
Harsh Naik <17bit027@nirmuni.ac.in>
2021-06-01 01:16:57 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+        var scroll=$(window).scrollTop();
+        if(scroll>=50){
+            $(".sticky").addClass("stickyadd");
+        }else{
+            $(".sticky").removeClass("stickyadd");
+        }
+
+    })
+
+    var typed= new Typed(".element",{
+        strings: ["Harsh Naik","a Software Developer" ],
+        smartBackspace:true,
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1000
+    });
+
+    // Progress Bars
+
+    
+
+    
+
+    var waypoint = new Waypoint({
+    element: document.getElementById('exp-id'),
+    handler: function() {
+        var p=document.querySelectorAll('.progress-bar');
+    p[0].setAttribute("style","width:90%;transition:1s all");
+    p[1].setAttribute("style","width:85%;transition:1.5 all");
+    p[2].setAttribute("style","width:75%;transition:1.7s all");
+    p[3].setAttribute("style","width:70%;transition:2s all");
+    p[4].setAttribute("style","width:60%;transition:2.3 all");
+      
+    },
+    offset:'90%'
+    });
+  
+  
+
+  
+    
+});
